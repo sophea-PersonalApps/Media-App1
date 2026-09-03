fff2a0d0f496a53143ffff8ee9cc4f654c5ba7a7
devLinguistPro <dev@linguistpro.app>
2026-09-04 05:32:10 +1000

    Set Media Toolbox version to 0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.devlinguistpro.mediatoolbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "0.4"
+        versionCode = 5
+        versionName = "0.5"
     }
 
     buildTypes {
