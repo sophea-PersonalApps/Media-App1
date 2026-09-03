a6f8fe24fe6b442e4325483230453b611f804138
devLinguistPro <dev@linguistpro.app>
2026-09-04 05:28:28 +1000

    Bump Media Toolbox to gallery editor version 0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.devlinguistpro.mediatoolbox"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.4"
     }
 
     buildTypes {
