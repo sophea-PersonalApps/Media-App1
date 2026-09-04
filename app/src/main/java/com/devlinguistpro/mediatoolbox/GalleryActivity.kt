2989f12fe726f01b83d552c5d33201807f44d663
devLinguistPro <dev@linguistpro.app>
2026-09-04 17:55:03 +1000

    Fix gallery Compose build import

--- app/src/main/java/com/devlinguistpro/mediatoolbox/GalleryActivity.kt
+++ app/src/main/java/com/devlinguistpro/mediatoolbox/GalleryActivity.kt
@@ -14,6 +14,7 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
